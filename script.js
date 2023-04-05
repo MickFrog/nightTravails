90be8f47bc16e12c2383697e6be1b6c96a7b6f1e
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-04-05 21:47:13 +0300

    write add edges functionality

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ function drawBoard() { //add the vertices (boxes on board)
         }
     }
 }         
+
+//add edges to all boxes in chessBoard
+function addEdges() {
+    //use the chessBoard
+    for(let [box] of chessBoard) {
+        const boxArr = box.split(',');
+        const x = parseInt(boxArr[0]);
+        const y = parseInt(boxArr[1]);
+
+        const KnightDirections = { // all knight directions from any box 
+            1: [ x + 1, y + 2 ],
+            2: [ x + 2, y + 1 ],
+            4: [ x + 2, y - 1 ],
+            5: [ x + 1, y - 2 ],
+            7: [ x - 1, y - 2 ],
+            8: [ x - 2, y - 1 ],
+            10: [ x - 2, y + 1 ],
+            11: [ x - 1, y + 2 ],
+        }
+
+        for (let dir in KnightDirections) {
+            const posMove = KnightDirections[dir].toString();
+
+            // if possible move (posMove) is valid on board and edge is not yet created on current box
+            if (chessBoard.has(posMove) && !chessBoard.get(box).includes(posMove)) {
+                chessBoard.get(box).push(posMove);
+            }
+        }
+    }
+}
